0fc20164fe41e75621552de646a8e49423b6db6b
leon gong <gongchenjie1@hotmail.com>
2020-11-28 20:56:26 +0800

    modify: 封装 Low filter Pass 函数。

--- SceneKitVehicle/GameVC+AccMeter.swift
+++ SceneKitVehicle/GameVC+AccMeter.swift
@@ -26,17 +26,27 @@ extension GameViewController{
     
     
     func accelerometerDidChange(_ acceleration: CMAcceleration) {
-        let kFilteringFactor = 0.5
+        let kLpFlt = 0.5
         
         //Use a basic low-pass filter to only keep the gravity in the accelerometer values
-        _accelerometer[0] = acceleration.x * kFilteringFactor + _accelerometer[0] * (1.0 - kFilteringFactor)
-        _accelerometer[1] = acceleration.y * kFilteringFactor + _accelerometer[1] * (1.0 - kFilteringFactor)
-        _accelerometer[2] = acceleration.z * kFilteringFactor + _accelerometer[2] * (1.0 - kFilteringFactor)
+
+
+        _accelerometer[0] = lowPassFilter(BaseData:_accelerometer[0], AccData: acceleration.x, Factor: kLpFlt)
+        _accelerometer[1] = lowPassFilter(BaseData:_accelerometer[1], AccData: acceleration.y, Factor: kLpFlt)
+        _accelerometer[2] = lowPassFilter(BaseData:_accelerometer[2], AccData: acceleration.z, Factor: kLpFlt)
+        
+        // 原来没有函数话前的代码， to be removed
+//        _accelerometer[1] = acceleration.y * kFilteringFactor + _accelerometer[1] * (1.0 - kFilteringFactor)
+//        _accelerometer[2] = acceleration.z * kFilteringFactor + _accelerometer[2] * (1.0 - kFilteringFactor)
         
         if _accelerometer[0] > 0 {
             _orientation = CGFloat(_accelerometer[1] * 1.3)
         } else {
             _orientation = -CGFloat(_accelerometer[1] * 1.3)
         }
+    }
+    
+    func lowPassFilter(BaseData:Double, AccData: Double, Factor:Double) -> Double{
+        return AccData * Factor + BaseData * (1.0 - Factor)
     }
 }
